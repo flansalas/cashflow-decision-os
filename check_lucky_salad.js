85d263a600d3daca0e03fafce43d12154f9bd2a0
Eduardo Salas Araujo <flans@MacBookPro.lan>
2026-09-06 10:05:09 -0400

    security: remove exposed database scripts and sensitive diagnostics

--- check_lucky_salad.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'postgresql://neondb_owner:npg_yN4JPm5dQLiv@ep-lucky-salad-anvg05zg-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require' });
-async function run() {
-  await client.connect();
-  const res = await client.query(`SELECT COUNT(*) FROM "BankTransaction" WHERE "companyId" = '1a7b36f5-8fe0-4c2b-9336-8420846270b5'`);
-  console.log('Count:', res.rows[0].count);
-  await client.end();
-}
-run().catch(console.error);
--- drop_migrations.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({ connectionString: 'postgresql://neondb_owner:npg_yN4JPm5dQLiv@ep-lucky-salad-anvg05zg-pooler.c-6.us-east-1.aws.neon.tech/neondb?sslmode=require' });
-async function run() {
-  await client.connect();
-  await client.query('TRUNCATE TABLE _prisma_migrations;');
-  console.log('Truncated _prisma_migrations');
-  await client.end();
-}
-run().catch(console.error);
